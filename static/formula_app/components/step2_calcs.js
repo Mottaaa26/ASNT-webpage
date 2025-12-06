03e753484c07e904339448bd3210d62039818dd3
Mottaaa26 <molinamottap@gmail.com>
2025-12-06 09:55:38 -0500

    Completado el cálculo del corrosion rate por alkaline sour water

--- static/formula_app/components/step2_calcs.js
+++ static/formula_app/components/step2_calcs.js
@@ -11,6 +11,7 @@ import { ht_sna_corrosion_calc } from "./modules-step2/ht_sna_corrosion_calcs.js
 import { ht_h2sh2_corrosion_calc } from "./modules-step2/ht_h2sh2_corrosion_calcs.js";
 import { sa_corrosion_calc } from "./modules-step2/sa_corrosion_calcs.js";
 import { hf_corrosion_calc } from "./modules-step2/hf_corrosion_calcs.js";
+import { alkaline_sw_corrosion_calc } from "./modules-step2/alkaline_sw_corrosion_calcs.js";
 
 // URL TO GET THE JSON WITH THE TABLE
 export const tables_data =
@@ -105,6 +106,9 @@ document.addEventListener("click", (e) => {
                         break;
                     case "hf_corrosion":
                         hf_corrosion_calc();
+                        break;
+                    case "alkaline_sw_corrosion":
+                        alkaline_sw_corrosion_calc();
                         break;
                     default:
                         break;
